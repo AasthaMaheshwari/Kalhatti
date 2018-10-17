e256cb8b38a5ba573601123067162a89f8c56736
Aastha Maheshwari <am07ruler@gmail.com>
2018-10-17 14:22:21 -0400

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -10,7 +10,7 @@ install_requires = [r.decode().split('==')[0] for r in reqs.split()]
 	if(install_requires)
 		install_packages == 1
 			print(success)
-	elif(!install_packages)
+	elif(install_packages)
 			print(install_requires)		
 	
 	
